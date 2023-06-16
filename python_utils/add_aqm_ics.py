17c7957e729e2d5b0c7020ee03a56ef67b9fbb8e
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-06-16 12:17:38 +0000

    ensure source attribute to final netcdf

--- python_utils/add_aqm_ics.py
+++ python_utils/add_aqm_ics.py
@@ -15,13 +15,21 @@ def add_aqm_tracers_ics(fv_tracer_file, wrk_ic_file):
     try: fv_tracer = xr.open_dataset(fv_tracer_file)
     except: raise Exception('Could NOT find the tracer file:',fv_tracer_file)
 
+    print('========== AQM tracer file:', fv_tracer)
+
     # Previous day atmosphere file
     try: gfs_atm = xr.open_dataset(wrk_ic_file)
     except: raise Exception('Could NOT find the IC file:',wrk_ic_file)
 
+    print('========== Original IC file W/O aqm tracers:', gfs_atm)
+
+    # Extract attributes of atmosphere file to ensure it in the final file
+    gfs_atm_attr_source = gfs_atm.attrs['source']
+
     # Remove time dimension and microphysics tracers from previous cycle's restart file
     ds_out = fv_tracer.drop({'Time','graupel','ice_wat','liq_wat',
                         'o3mr','rainwat','snowwat','sphum'})
+
     ds_out = ds_out.squeeze('Time')
 
     # Rename dimensions and coordinates from restart file to match names in atmosphere's IC file
@@ -31,6 +39,7 @@ def add_aqm_tracers_ics(fv_tracer_file, wrk_ic_file):
     # the restart file. We extract the top layer from the tracer file 
     # and extend the number of vertical levels by 1 (i.e. from 64 to 65) 
     ds_out2 = ds_out.isel(lev=slice(1))
+
     ds_new = xr.concat([ds_out2,ds_out],dim='lev')
 
     # Rebuild the lev values from 1 to 65
@@ -38,14 +47,22 @@ def add_aqm_tracers_ics(fv_tracer_file, wrk_ic_file):
 
     # Add the vertically-extended tracers to the GFS IC file
     ds_new2 = xr.merge([gfs_atm,ds_new],join='left')
+
     ds_new2 = ds_new2.drop({'lev','lat','lon'})
+
+    # Add source attribute to the final file
+    ds_news2 = ds_new2.attrs['source'] = gfs_atm_attr_source
+
+    print('========== New IC file with AQM tracers (tmp1.nc):', ds_new2)
 
     # Output to a temporary file for the later steps
     try: ds_new2.to_netcdf('tmp1.nc')
     except: raise Exception('Error in creating output file tmp1.nc')
 
+    print('===========================================================')
     print('The intermediate file tmp1.nc has been created successfully')
-     
+    print('===========================================================')
+
     return True
 
 
